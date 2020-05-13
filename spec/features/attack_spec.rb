bbf5c732c588868c2de4c086d3c1da09546af73a
Anna <anna.voitik@gmail.com>
2020-05-13 11:49:58 +0100

    Variables issue resolved, tests pass

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,8 +8,7 @@ feature 'Attacking' do
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    click_link 'OK'
-    expect(page).not_to have_content 'Mittens: 60HP'
-    expect(page).to have_content 'Mittens: 50HP'
+    expect(page).not_to have_content 'Mittens: 60 HP'
+    expect(page).to have_content 'Mittens: 50 HP'
   end
 end
